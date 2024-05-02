908dc56fd3c1623dd53b5ad5cd58d3d0a720c39e
“patrickpiedad” <patrickpiedad@gmail.com>
2024-05-02 05:32:23 +0200

    Added 02MAY24 codeWars

---
+++ codeWars_02MAY24/js/main.js
@@ -0,0 +1,33 @@
+//codeWars DDMMMYY
+
+//TASK
+//Your task is to make two functions ( max and min, or maximum and minimum, etc., depending on the language ) that receive a list of integers as input,
+// and return the largest and lowest number in that list, respectively.
+//
+// Examples (Input -> Output)
+// * [4,6,2,1,9,63,-134,566]         -> max = 566, min = -134
+// * [-52, 56, 30, 29, -54, 0, -110] -> min = -110, max = 56
+// * [42, 54, 65, 87, 0]             -> min = 0, max = 87
+// * [5]                             -> min = 5, max = 5
+// Notes
+// You may consider that there will not be any empty arrays/vectors.
+
+
+//PSEUDOCODE
+// get input of an array of numbers
+// use Math.min / Math.max on array and assign to variable
+// may have to use (...arr) in syntax of Math methods
+
+
+//SOLUTION
+let min = function(list){
+    return Math.min(...list)
+}
+
+let max = function(list){
+    return Math.max(...list)
+}
+
+// BEST SOLUTION
+// const min = (list) => Math.min(...list);
+// const max = (list) => Math.max(...list);
