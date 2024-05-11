11e43d37eb3270b3ed78812b92a9a7185e67b133
patrickpiedad <patrickpiedad@gmail.com>
2024-05-11 21:55:33 +0200

    Changing files back to only JS to save space.Using Quokka extension to test in VSCode.

--- codeWars_10MAY24.js
+++ codeWars_10MAY24.js
@@ -12,29 +12,22 @@
 // The number can be negative already, in which case no change is required.
 // Zero (0) is not checked for any specific sign. Negative zeros make no mathematical sense.
 
-
 //PSEUDOCODE
 // get number input
-// if number < 0, return number. 
+// if number < 0, return number.
 // if number > 0, Use Math.abs and multiply by -1
 // if number = 0, return 0
 
-
 //SOLUTION
 function makeNegative(num) {
-    if (num === 0){
-        return 0
-    }else if (num < 0){
-        return num
-    } else
-        return (Math.abs(num) * -1)
-  }
-
-
-
+	if (num === 0) {
+		return 0
+	} else if (num < 0) {
+		return num
+	} else return Math.abs(num) * -1
+}
 
 // BEST SOLUTION
 // function makeNegative(num) {
 //     return -Math.abs(num);
 //   }
-
