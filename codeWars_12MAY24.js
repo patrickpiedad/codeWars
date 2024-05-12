74cd1c1824975515b5d1f7d3d92a1c12e9117ee0
patrickpiedad <patrickpiedad@gmail.com>
2024-05-12 21:49:33 +0200

    Completed extra codeWars on 12MAY24

--- codeWars_12MAY24.js
+++ codeWars_12MAY24.js
@@ -27,3 +27,43 @@ repeatStr(5, 'i')
 // function repeatStr (n, s) {
 //     return s.repeat(n);
 //   }
+
+//codeWars 12MAY24
+
+//TASK
+//Rock Paper Scissors
+// Let's play! You have to return which player won! In case of a draw return Draw!.
+
+// Examples(Input1, Input2 --> Output):
+
+// "scissors", "paper" --> "Player 1 won!"
+// "scissors", "rock" --> "Player 2 won!"
+// "paper", "paper" --> "Draw!"
+
+// Examples (input -> output)
+// 6, "I"     -> "IIIIII"
+// 5, "Hello" -> "HelloHelloHelloHelloHello"
+
+//PSEUDOCODE
+// if conditional for each use case, return string
+
+//SOLUTION
+const rps = (p1, p2) => {
+	if (p1 === 'scissors' && p2 === 'paper') {
+		return 'Player 1 won!'
+	} else if (p1 === 'scissors' && p2 === 'rock') {
+		return 'Player 2 won!'
+	} else if (p1 === p2) {
+		return 'Draw!'
+	} else if (p1 === 'rock' && p2 === 'scissors') {
+		return 'Player 1 won!'
+	} else if (p1 === 'rock' && p2 === 'paper') {
+		return 'Player 2 won!'
+	} else if (p1 === 'paper' && p2 === 'scissors') {
+		return 'Player 2 won!'
+	} else if (p1 === 'paper' && p2 === 'rock') {
+		return 'Player 1 won!'
+	}
+}
+
+//BEST SOLUTION
