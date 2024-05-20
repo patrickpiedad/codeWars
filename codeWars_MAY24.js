d6ef1e52c9f1249a60f5544ffeafd4e45dabc3df
patrickpiedad <patrickpiedad@gmail.com>
2024-05-20 08:30:52 +0200

    Added 20MAY24 codeWars

--- codeWars_MAY24.js
+++ codeWars_MAY24.js
@@ -701,3 +701,49 @@
 // console.log(sumTwoSmallestNumbers([10, 343445353, 3453445, 3453545353453]))
 
 // BEST SOLUTION
+
+///////////////////////////////codeWars 20MAY24///////////////////////////////
+
+//TASK
+// Given two integers a and b, which can be positive or negative, find the sum of all the integers between and including them and return it. If the two numbers are equal return a or b.
+
+// Note: a and b are not ordered!
+
+// Examples (a, b) --> output (explanation)
+// (1, 0) --> 1 (1 + 0 = 1)
+// (1, 2) --> 3 (1 + 2 = 3)
+// (0, 1) --> 1 (0 + 1 = 1)
+// (1, 1) --> 1 (1 since both are same)
+// (-1, 0) --> -1 (-1 + 0 = -1)
+// (-1, 2) --> 2 (-1 + 0 + 1 + 2 = 2)
+// Your function should only return a number, not the explanation about how you get that number.
+
+//PSEUDOCODE
+// get integers a and b
+// if a < b --> start at a // else start at b
+
+//SOLUTION
+// function getSum(a, b) {
+// 	let sum = 0
+// 	if (a < b) {
+// 		for (let i = a; i <= b; i++) {
+// 			sum += i
+// 		}
+// 	} else if (b < a) {
+// 		for (let i = b; i <= a; i++) {
+// 			sum += i
+// 		}
+// 	} else if (a === b) {
+// 		return a
+// 	}
+// 	return sum
+// }
+
+// console.log(getSum(-1,2))
+
+// BEST SOLUTION
+// const GetSum = (a, b) => {
+//     let min = Math.min(a, b),
+//         max = Math.max(a, b);
+//     return (max - min + 1) * (min + max) / 2;
+//   }
