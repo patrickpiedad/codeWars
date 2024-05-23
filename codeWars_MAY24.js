1fbab4843f01b4a60ddee814135a404113f49eb6
patrickpiedad <patrickpiedad@gmail.com>
2024-05-23 07:20:03 +0200

    Added 23MAY24 codewars, did not complete because of mutation error

--- codeWars_MAY24.js
+++ codeWars_MAY24.js
@@ -778,10 +778,54 @@
 //PSEUDOCODE
 
 //SOLUTION
-class Kata {
-	static getVolumeOfCuboid(length, width, height) {
-		return length * width * height
-	}
-}
+// class Kata {
+// 	static getVolumeOfCuboid(length, width, height) {
+// 		return length * width * height
+// 	}
+// }
 
 // BEST SOLUTION
+
+///////////////////////////////codeWars 23MAY24///////////////////////////////
+
+//TASK
+// The museum of incredibly dull things
+// The museum of incredibly dull things wants to get rid of some exhibits.
+//Miriam, the interior architect, comes up with a plan to remove the most boring exhibits. She gives them a rating, and then removes the one with the lowest rating.
+
+// However, just as she finished rating all exhibits, she's off to an important fair, so she asks you to write a
+//program that tells her the ratings of the exhibits after removing the lowest one. Fair enough.
+
+// Task
+// Given an array of integers, remove the smallest value. Do not mutate the original array/list.
+//If there are multiple elements with the same value, remove the one with the lowest index. If you get an empty array/list, return an empty array/list.
+
+// Don't change the order of the elements that are left.
+
+// Examples
+// * Input: [1,2,3,4,5], output = [2,3,4,5]
+// * Input: [5,3,2,1,4], output = [5,3,2,4]
+// * Input: [2,2,1,2,1], output = [2,2,2,1]
+
+//PSEUDOCODE
+// get array
+// use .map to generate new array OR just create new array equal to original array
+// use .min() to find minimum value
+// use .pop() to remove it
+// return array
+
+//SOLUTION
+// function removeSmallest(numbers) {
+// 	let minValIndex = numbers.indexOf(Math.min(...numbers))
+// 	let newArr = numbers
+// 	newArr.splice(minValIndex, 1)
+// 	return newArr
+// }
+
+// removeSmallest([5, 3, 2, 1, 4])
+
+// BEST SOLUTION
+// function removeSmallest(numbers) {
+// 	let indexOfMin = numbers.indexOf(Math.min(...numbers))
+// 	return [...numbers.slice(0, indexOfMin), ...numbers.slice(indexOfMin + 1)]
+// }
