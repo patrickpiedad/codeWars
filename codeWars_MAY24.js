000da3577788f35fd6c33d124892543d8a74991f
patrickpiedad <patrickpiedad@gmail.com>
2024-05-24 06:32:08 +0200

    Added 24MAY24 codewars, failed to complete

--- codeWars_MAY24.js
+++ codeWars_MAY24.js
@@ -829,3 +829,45 @@
 // 	let indexOfMin = numbers.indexOf(Math.min(...numbers))
 // 	return [...numbers.slice(0, indexOfMin), ...numbers.slice(indexOfMin + 1)]
 // }
+
+///////////////////////////////codeWars 24MAY24///////////////////////////////
+
+//TASK
+// Welcome. In this kata, you are asked to square every digit of a number and concatenate them.
+
+// For example, if we run 9119 through the function, 811181 will come out, because 92 is 81 and 12 is 1. (81-1-1-81)
+
+// Example #2: An input of 765 will/should return 493625 because 72 is 49, 62 is 36, and 52 is 25. (49-36-25)
+
+// Note: The function accepts an integer and returns an integer.
+
+// Happy Coding!
+
+//PSEUDOCODE
+// get num, .split(), foreach square, concat, return
+
+//SOLUTION
+// function squareDigits(num) {
+// 	let digits = num.toString().split('')
+// 	let numArr = digits.map(Number)
+// 	let ans
+// 	for (i = 0; i < numArr.length; i++) {
+// 		numArr[i] = numArr[i] * numArr[i]
+// 		ans += numArr[i].toString()
+// 	}
+// 	ans = Number
+// 	console.log(ans)
+// }
+// console.log(squareDigits)
+
+// squareDigits(9119)
+
+// BEST SOLUTION
+// function squareDigits(num){
+//     let string = num.toString();
+//     let results = [];
+//     for (let i = 0; i < string.length; i++){
+//         results[i] = string[i] * string[i];
+//     }
+//     return Number(results.join(''));
+// };
