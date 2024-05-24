22482a5e07be5f3382126e4a894766370aa5192d
patrickpiedad <patrickpiedad@gmail.com>
2024-05-24 06:58:16 +0200

    added another 24MAY24 codewars

--- codeWars_MAY24.js
+++ codeWars_MAY24.js
@@ -871,3 +871,28 @@
 //     }
 //     return Number(results.join(''));
 // };
+
+//TASK
+// Given a string, you have to return a string in which each character (case-sensitive) is repeated once.
+
+// Examples (Input -> Output):
+// * "String"      -> "SSttrriinngg"
+// * "Hello World" -> "HHeelllloo  WWoorrlldd"
+// * "1234!_ "     -> "11223344!!__  "
+// Good Luck!
+
+//PSEUDOCODE
+
+//SOLUTION
+function doubleChar(str) {
+	return str
+		.split('')
+		.map(function (letter) {
+			return letter + letter
+		})
+		.join('')
+}
+
+console.log(doubleChar('String'))
+
+// BEST SOLUTION
