8d21b20656e296e0526aa38533b03a0ee92f0f6d
patrickpiedad <patrickpiedad@gmail.com>
2024-05-28 05:19:25 +0200

    added 28MAY24 codewars

--- codeWars_MAY24.js
+++ codeWars_MAY24.js
@@ -993,3 +993,54 @@
 // function minMax(arr){
 //     return [Math.min(...arr), Math.max(...arr)];
 //   }
+
+///////////////////////////////codeWars 28MAY24///////////////////////////////
+
+//TASK
+// In a factory a printer prints labels for boxes. For one kind of boxes the printer has to use colors which, for the sake of simplicity, are named with letters from a to m.
+
+// The colors used by the printer are recorded in a control string. For example a "good" control string would be aaabbbbhaijjjm meaning that the printer used three times color a, four times color b, one time color h then one time color a...
+
+// Sometimes there are problems: lack of colors, technical malfunction and a "bad" control string is produced e.g. aaaxbbbbyyhwawiwjjjwwm with letters not from a to m.
+
+// You have to write a function printer_error which given a string will return the error rate of the printer as a string representing a rational whose numerator is the number of errors and the denominator the length of the control string. Don't reduce this fraction to a simpler expression.
+
+// The string has a length greater or equal to one and contains only letters from ato z.
+
+//PSEUDOCODE
+// Get input, convert to array, use for loop to iterate through each letter
+// conditonal, if index is equal to a, b, c, d, e, f, g, h, i, j, k, l, m then iterate "x" +1
+// else, continue to next index
+// return 'x / arr.length'
+
+//SOLUTION
+function printerError(s) {
+	let str = s.split('')
+	let x = 0
+	for (let i = 0; i < str.length; i++)
+		if (
+			str[i] === 'a' ||
+			str[i] === 'b' ||
+			str[i] === 'c' ||
+			str[i] === 'd' ||
+			str[i] === 'e' ||
+			str[i] === 'f' ||
+			str[i] === 'g' ||
+			str[i] === 'h' ||
+			str[i] === 'i' ||
+			str[i] === 'j' ||
+			str[i] === 'k' ||
+			str[i] === 'l' ||
+			str[i] === 'm'
+		) {
+			x += 1
+		} else {
+			x = x
+		}
+	let error = str.length - x
+	return `${error}/${str.length}`
+}
+
+console.log(printerError('aaaxbbbbyyhwawiwjjjwwm'))
+
+// BEST SOLUTION
