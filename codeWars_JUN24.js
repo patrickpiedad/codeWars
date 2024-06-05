0a0705bc577abe2fd0a796805745269487c91f15
patrickpiedad <patrickpiedad@gmail.com>
2024-06-05 05:27:41 +0200

    added 05JUN24 codewars

--- codeWars_JUN24.js
+++ codeWars_JUN24.js
@@ -193,17 +193,60 @@
 // 	}
 // }
 
-function duplicateCount(text) {
-	let arr = text.toLowerCase().split('').sort()
-	let duplArr = []
+// function duplicateCount(text) {
+// 	let arr = text.toLowerCase().split('').sort()
+// 	let duplArr = []
 
-	for (let i = 0; i < arr.length; i++) {
-		if (arr[i] === arr[i + 1] && !duplArr.includes(arr[i])) {
-			duplArr.push(arr[i])
-		}
-	}
-	return duplArr.length
-}
+// 	for (let i = 0; i < arr.length; i++) {
+// 		if (arr[i] === arr[i + 1] && !duplArr.includes(arr[i])) {
+// 			duplArr.push(arr[i])
+// 		}
+// 	}
+// 	return duplArr.length
+// }
 
-console.log(duplicateCount('Indivisibilities'))
+// console.log(duplicateCount('Indivisibilities'))
 // BEST SOLUTION
+
+///////////////////////////////codeWars 05JUN24///////////////////////////////
+
+//TASK
+// There is a bus moving in the city which takes and drops some people at each bus stop.
+
+// You are provided with a list (or array) of integer pairs. Elements of each pair represent the number of people that get on the bus (the first item) and the number of people that get off the bus (the second item) at a bus stop.
+
+// Your task is to return the number of people who are still on the bus after the last bus stop (after the last array). Even though it is the last bus stop, the bus might not be empty and some people might still be inside the bus, they are probably sleeping there :D
+
+// Take a look on the test cases.
+
+// Please keep in mind that the test cases ensure that the number of people in the bus is always >= 0. So the returned integer can't be negative.
+
+// The second value in the first pair in the array is 0, since the bus is empty in the first bus stop.
+
+//PSEUDOCODE
+// set starting value of people on the bus as 0
+// loop through each pair of array values, adding the first element and subtracting the second element
+// simplify all array pairs by reducing to one number, then add all of those
+
+//SOLUTION
+// const number = function (busStops) {
+// 	let numPassengers = 0
+// 	for (let i = 0; i < busStops.length; i++) {
+// 		numPassengers = numPassengers + (busStops[i][0] - busStops[i][1])
+// 	}
+// 	return numPassengers
+// }
+
+// console.log(
+// 	number([
+// 		[3, 0],
+// 		[9, 1],
+// 		[4, 10],
+// 		[12, 2],
+// 		[6, 1],
+// 		[7, 10],
+// 	])
+// )
+
+// BEST SOLUTION
+// const number = (busStops) => busStops.reduce((rem, [on, off]) => rem + on - off, 0);
