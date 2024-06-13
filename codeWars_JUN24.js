5fce057a985b7a947790073e2488c2c5958b1f36
patrickpiedad <patrickpiedad@gmail.com>
2024-06-13 22:11:36 +0200

    working on new codewars 13JUN2024

--- codeWars_JUN24.js
+++ codeWars_JUN24.js
@@ -518,3 +518,27 @@
 //   }
 
 // BEST SOLUTION
+
+//TASK
+// You are given an odd-length array of integers, in which all of them are the same, except for one single number.
+
+// Complete the method which accepts such an array, and returns that single different number.
+
+// The input array will always be valid! (odd-length >= 3)
+
+// Examples
+// [1, 1, 2] ==> 2
+// [17, 17, 3, 17, 17, 17, 17] ==> 3
+
+//PSEUDOCODE
+// get array
+// use spread operator and .set method to get unique values
+
+//SOLUTION
+function stray(numbers) {
+	return [...new Set(numbers)]
+}
+
+console.log(stray([17, 17, 3, 17, 17, 17, 17, 17]))
+
+// BEST SOLUTION
