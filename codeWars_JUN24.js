5352a05dc319477eedf03aa6d0da6036a30aa1c0
patrickpiedad <patrickpiedad@gmail.com>
2024-06-15 08:29:03 +0200

    added 15JUN2024 codewars, edited HTML and index HTML files to reflect better descriptions in preparetion for app creation

--- codeWars_JUN24.js
+++ codeWars_JUN24.js
@@ -555,3 +555,48 @@
 // 		}
 // 	}
 // }
+
+///////////////////////////////codeWars 15JUN2024///////////////////////////////
+
+//TASK
+// Write function bmi that calculates body mass index (bmi = weight / (height*height)).
+
+// if bmi <= 18.5 return "Underweight"
+
+// if bmi <= 25.0 return "Normal"
+
+// if bmi <= 30.0 return "Overweight"
+
+// if bmi > 30 return "Obese"
+
+//PSEUDOCODE
+// get weight and height inputs
+// let bmi = weight * height
+// if bmi <= 18.5 return "Underweight"
+// if bmi <= 25.0 return "Normal"
+// if bmi <= 30.0 return "Overweight"
+// if bmi > 30 return "Obese"
+// use switch case
+
+//SOLUTION
+function bmi(weight, height) {
+	let bmi = weight / (height * height)
+	switch (true) {
+		case bmi <= 18.5:
+			return 'Underweight'
+			break
+		case bmi <= 25.0:
+			return 'Normal'
+			break
+		case bmi <= 30.0:
+			return 'Overweight'
+			break
+		case bmi > 30:
+			return 'Obese'
+			break
+	}
+}
+
+console.log(bmi(80, 1.8))
+
+// BEST SOLUTION
