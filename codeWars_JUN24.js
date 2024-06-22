7073d8008f095617cdfd7258f834ccbd8ca3504f
patrickpiedad <patrickpiedad@gmail.com>
2024-06-22 09:36:02 +0200

    adding 22JUN2024 codewars

--- codeWars_JUN24.js
+++ codeWars_JUN24.js
@@ -697,4 +697,39 @@
 
 // BEST SOLUTION
 
-// Learning OOP with Mosh//
+///////////////////////////////codeWars 22JUN2024///////////////////////////////
+
+//TASK
+// Character recognition software is widely used to digitise printed texts. Thus the texts can be edited, searched and stored on a computer.
+
+// When documents (especially pretty old ones written with a typewriter), are digitised character recognition softwares often make mistakes.
+
+// Your task is correct the errors in the digitised text. You only have to handle the following mistakes:
+
+// S is misinterpreted as 5
+// O is misinterpreted as 0
+// I is misinterpreted as 1
+// The test cases contain numbers only by mistake.
+
+//PSEUDOCODE
+
+//SOLUTION
+// function correct(string) {
+// 	string = string.split('')
+// 	console.log(string)
+// 	console.log(string.length)
+// 	for (let i = 0; i < string.length; i++) {
+// 		if (string[i] === '5') {
+// 			string[i] = 'S'
+// 		} else if (string[i] === '0') {
+// 			string[i] = 'O'
+// 		} else if (string[i] === '1') {
+// 			string[i] = 'I'
+// 		}
+// 	}
+// 	return string.join('')
+// }
+
+// console.log(correct('L0ND0N'))
+
+// BEST SOLUTION
