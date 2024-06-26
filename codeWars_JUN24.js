5118b54f07d25e889c7bde9d1cea0727448ab41f
patrickpiedad <patrickpiedad@gmail.com>
2024-06-26 05:06:10 +0200

    comnpleted 26JUN2024 codewars, online codewars is broken due to time out test

--- codeWars_JUN24.js
+++ codeWars_JUN24.js
@@ -797,3 +797,38 @@
 // }
 
 // BEST SOLUTION
+
+///////////////////////////////codeWars 26JUN2024///////////////////////////////
+
+//TASK
+// Count the number of divisors of a positive integer n.
+
+// Random tests go up to n = 500000.
+
+// Examples (input --> output)
+// 4 --> 3 // we have 3 divisors - 1, 2 and 4
+// 5 --> 2 // we have 2 divisors - 1 and 5
+// 12 --> 6 // we have 6 divisors - 1, 2, 3, 4, 6 and 12
+// 30 --> 8 // we have 8 divisors - 1, 2, 3, 5, 6, 10, 15 and 30
+// Note you should only return a number, the count of divisors. The numbers between parentheses are shown only for you to see which numbers are counted in each case.
+
+//PSEUDOCODE
+// take in positive integer n
+// for loop, using i and n to figure out the problem, n % i === 0
+// add one to divisor count
+// return divisor count
+
+//SOLUTION
+function getDivisorsCnt(n) {
+	let divisorCount = 0
+	for (let i = 1; i <= n; i++) {
+		if (n % i === 0) {
+			divisorCount++
+		}
+	}
+	return divisorCount
+}
+
+console.log(getDivisorsCnt())
+
+// BEST SOLUTION
