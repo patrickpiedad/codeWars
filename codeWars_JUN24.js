080056f3e532bcd92dbc5282472004157ebe310b
Patrick Piedad <patrickpiedad@gmail.com>
2024-06-26 08:23:37 +0200

    commented out code, testing github upload with new windows system

--- codeWars_JUN24.js
+++ codeWars_JUN24.js
@@ -837,10 +837,10 @@
 //PSEUDOCODE
 
 //SOLUTION
-function noSpace(x){
-	return x.replaceAll(' ', '')
-}
+// function noSpace(x){
+// 	return x.replaceAll(' ', '')
+// }
 
-console.log(noSpace("8 j 8   mBliB8g  imjB8B8  jl  B"))
+// console.log(noSpace("8 j 8   mBliB8g  imjB8B8  jl  B"))
 
 // BEST SOLUTION
