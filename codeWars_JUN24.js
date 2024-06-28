e38527af9772103216e09a30a0de4a9a046b6648
patrickpiedad <patrickpiedad@gmail.com>
2024-06-28 05:27:48 +0200

    fixing rebase

--- codeWars_JUN24.js
+++ codeWars_JUN24.js
@@ -884,3 +884,40 @@
 // function fakeBin(x) {
 //     return x.split('').map(n => n < 5 ? 0 : 1).join('');
 // }
+
+///////////////////////////////codeWars 28JUN2024///////////////////////////////
+
+//TASK
+// Given an array of integers.
+
+// Return an array, where the first element is the count of positives numbers and the second element is sum of negative numbers. 0 is neither positive nor negative.
+
+// If the input is an empty array or is null, return an empty array.
+
+// Example
+// For input [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, -11, -12, -13, -14, -15], you should return [10, -65].
+
+//PSEUDOCODE
+
+//SOLUTION
+function countPositivesSumNegatives(input) {
+	let posCount = 0
+	let negSum = 0
+	let newArr = []
+	if (input === null || input.reduce((acc, currVal) => acc + currVal, 0) === 0) {
+		return []
+	} else {
+		for (let i = 0; i < input.length; i++) {
+			if (input[i] > 0) {
+				posCount++
+			} else if (input[i] < 0) {
+				negSum += input[i]
+			}
+		}
+	}
+	newArr.push(posCount)
+	newArr.push(negSum)
+	return newArr
+}
+
+// BEST SOLUTION
