dea2176cb970d9e550b82d41c1f12fdf083bbb74
patrickpiedad <patrickpiedad@gmail.com>
2024-06-28 05:24:22 +0200

    adding codewars 28JUN24, not complete

--- codeWars_JUN24.js
+++ codeWars_JUN24.js
@@ -819,7 +819,6 @@
 // return divisor count
 
 //SOLUTION
- 
 
 // BEST SOLUTION
 
@@ -884,4 +883,5 @@
 // BEST SOLUTION
 // function fakeBin(x) {
 //     return x.split('').map(n => n < 5 ? 0 : 1).join('');
+>>>>>>> 7d703ee (added 27JUN2024 codewars 8kyu)
 // }
