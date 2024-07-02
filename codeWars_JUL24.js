473dc2698b3a82ce25b87523a6c2d39a0c04b875
patrickpiedad <patrickpiedad@gmail.com>
2024-07-02 05:08:31 +0200

    added 02JUL2024 codewars

--- codeWars_JUL24.js
+++ codeWars_JUL24.js
@@ -10,8 +10,54 @@
 //PSEUDOCODE
 
 //SOLUTION
-let hero = (bullets, dragons) => (bullets / 2 >= dragons ? true : false)
+// let hero = (bullets, dragons) => (bullets / 2 >= dragons ? true : false)
 
-console.log(hero(10, 5))
+// console.log(hero(10, 5))
 
 // BEST SOLUTION
+
+///////////////////////////////codeWars 02JUL2024///////////////////////////////
+
+//TASK
+// Check to see if a string has the same amount of 'x's and 'o's. The method must return a boolean and be case insensitive. The string can contain any char.
+
+// Examples input/output:
+
+// XO("ooxx") => true
+// XO("xooxx") => false
+// XO("ooxXm") => true
+// XO("zpzpzpp") => true // when no 'x' and 'o' is present should return true
+// XO("zzoo") => false
+
+//PSEUDOCODE
+// receive string from argument
+// create variable for counting x's and o's
+// make string lower case, split into array, loop through and iterate x and o variables as needed
+// if variable x and o are equal, return true, else return false
+
+//SOLUTION
+// function XO(str) {
+// 	let xCount = 0
+// 	let oCount = 0
+// 	let strArr = str.toLowerCase().split('')
+// 	console.log(strArr)
+// 	for (let i = 0; i < strArr.length; i++) {
+// 		if (strArr[i] === 'x') {
+// 			xCount++
+// 		} else if (strArr[i] === 'o') {
+// 			oCount++
+// 		}
+// 	}
+// 	if (xCount === oCount) {
+// 		return true
+// 	} else {
+// 		return false
+// 	}
+// }
+
+// console.log(XO('ooxx'))
+
+// BEST SOLUTION
+// function XO(str) {
+//     return str.toLowerCase().split('x').length === str.toLowerCase().split('o').length;
+// }
