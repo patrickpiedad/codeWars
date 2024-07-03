4a71dd8a1a0294b5ff841483414616240cf51052
patrickpiedad <patrickpiedad@gmail.com>
2024-07-03 05:55:26 +0200

    added 03JUL2024 codewars

--- codeWars_JUL24.js
+++ codeWars_JUL24.js
@@ -61,3 +61,20 @@
 // function XO(str) {
 //     return str.toLowerCase().split('x').length === str.toLowerCase().split('o').length;
 // }
+
+///////////////////////////////codeWars 03JUL2024///////////////////////////////
+
+//TASK
+// Alex just got a new hula hoop, he loves it but feels discouraged because his little brother is better than him.
+
+// Write a program where Alex can input (n) how many times the hoop goes round and it will return him an encouraging message:
+
+// If Alex gets 10 or more hoops, return the string "Great, now move on to tricks".
+// If he doesn't get 10 hoops, return the string "Keep at it until you get it".
+
+//PSEUDOCODE
+
+//SOLUTION
+let hoopCount = n => (n >= 10 ? 'Great now move on to tricks' : 'Keep at it until you get it')
+
+// BEST SOLUTION
