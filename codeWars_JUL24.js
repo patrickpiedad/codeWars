21c54e6b0b2cedd8e4b7384177daf93f25b87783
patrickpiedad <patrickpiedad@gmail.com>
2024-07-04 04:43:05 +0200

    added 04JUL2024 codewars

--- codeWars_JUL24.js
+++ codeWars_JUL24.js
@@ -75,6 +75,53 @@
 //PSEUDOCODE
 
 //SOLUTION
-let hoopCount = n => (n >= 10 ? 'Great now move on to tricks' : 'Keep at it until you get it')
+// let hoopCount = n => (n >= 10 ? 'Great, now move on to tricks' : 'Keep at it until you get it')
 
 // BEST SOLUTION
+
+///////////////////////////////codeWars 04JUL2024///////////////////////////////
+
+//TASK
+// Create a function finalGrade, which calculates the final grade of a student depending on two parameters: a grade for the exam and a number of completed projects.
+
+// This function should take two arguments: exam - grade for exam (from 0 to 100); projects - number of completed projects (from 0 and above);
+
+// This function should return a number (final grade). There are four types of final grades:
+
+// 100, if a grade for the exam is more than 90 or if a number of completed projects more than 10.
+// 90, if a grade for the exam is more than 75 and if a number of completed projects is minimum 5.
+// 75, if a grade for the exam is more than 50 and if a number of completed projects is minimum 2.
+// 0, in other cases
+// Examples(Inputs-->Output):
+
+// 100, 12 --> 100
+// 99, 0 --> 100
+// 10, 15 --> 100
+
+// 85, 5 --> 90
+
+// 55, 3 --> 75
+
+// 55, 0 --> 0
+
+//PSEUDOCODE
+
+//SOLUTION
+// function finalGrade(exam, projects) {
+// 	if (exam > 90 || projects > 10) {
+// 		return 100
+// 	} else if (exam > 75 && projects >= 5) {
+// 		return 90
+// 	} else if (exam > 50 && projects >= 2) {
+// 		return 75
+// 	} else {
+// 		return 0
+// 	}
+// }
+// BEST SOLUTION
+// function finalGrade (exam, projects) {
+//     if(exam > 90 || projects > 10) return 100;
+//     if(exam > 75 & projects >= 5) return 90;
+//     if(exam > 50 & projects >= 2) return 75;
+//     return 0;
+//   }
