3d7ff4d793035ead2d94f20fb291664cd4e97a3b
patrickpiedad <patrickpiedad@gmail.com>
2024-07-05 12:29:33 +0200

    added 05JUL24 codewars

--- codeWars_JUL24.js
+++ codeWars_JUL24.js
@@ -125,3 +125,27 @@
 //     if(exam > 50 & projects >= 2) return 75;
 //     return 0;
 //   }
+
+///////////////////////////////codeWars 05JUL2024///////////////////////////////
+
+//TASK
+// Nathan loves cycling.
+
+// Because Nathan knows it is important to stay hydrated, he drinks 0.5 litres of water per hour of cycling.
+
+// You get given the time in hours and you need to return the number of litres Nathan will drink, rounded to the smallest value.
+
+// For example:
+
+// time = 3 ----> litres = 1
+
+// time = 6.7---> litres = 3
+
+// time = 11.8--> litres = 5
+
+//PSEUDOCODE
+
+//SOLUTION
+// let litres = time => Math.floor(0.5 * time)
+
+// BEST SOLUTION
