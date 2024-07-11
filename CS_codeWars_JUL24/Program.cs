9d1aa2e0feb32428544dda857f0e8746fc37d1a3
patrickpiedad <patrickpiedad@gmail.com>
2024-07-11 22:01:51 +0200

    adding CSharp JUL2024 file, working through template for pushing daily problems.

---
+++ CS_codeWars_JUL24/Program.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+// TEMPLATE
+
+///////////////////////////////codeWars DDMMMYY///////////////////////////////
+
+// TASK
+
+// PARAMETERS
+
+// RETURNS
+
+// EXAMPLES
+
+// PSEUDOCODE
+
+// SOLUTION
+
+// BEST SOLUTION
+
+namespace CS_codeWars_JUL24
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Insert function to be tested here
+        }
+
+        ///////////////////////////////codeWars 11JUL2024///////////////////////////////
+
+        // TASK
+        //This code should store "codewa.rs" as a variable called name but it's not working. Can you figure out why?
+
+        //public class Kata
+        //{
+        //    public static string A = "code";
+        //    public static string B = "wa.rs";
+        //    public static string Name = "A + B";
+        //}
+
+        // PARAMETERS
+        // no parameters, only code fixing
+
+        // RETURNS
+        // no returns, only code fixing
+
+        // EXAMPLES
+        // no examples, only code fixing
+
+        // PSEUDOCODE
+        // check if each variable is initialized properly
+
+        // SOLUTION
+        public class Kata
+        {
+            public static string A = "code";
+            public static string B = "wa.rs";
+            public static string Name = A + B;
+        }
+
+        // BEST SOLUTION
+    }
+}
