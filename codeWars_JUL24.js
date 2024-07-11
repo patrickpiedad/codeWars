bf36d4ad8cb84efec00d971d1920f71461f92dfd
patrickpiedad <patrickpiedad@gmail.com>
2024-07-11 15:03:37 +0200

    adding another codewars for 11JUL2024

--- codeWars_JUL24.js
+++ codeWars_JUL24.js
@@ -468,3 +468,45 @@
 // 	}
 // 	return longest
 // }
+
+// TASK
+// Note: This kata is inspired by Convert a Number to a String!. Try that one too.
+
+// Description
+// We need a function that can transform a string into a number. What ways of achieving this do you know?
+
+// Note: Don't worry, all inputs will be strings, and every string is a perfectly valid representation of an integral number.
+
+// Examples
+// "1234" --> 1234
+// "605"  --> 605
+// "1405" --> 1405
+// "-7" --> -7
+
+// PARAMETERS
+// all inputs "str" will be a string
+// "str" will be a one for one representation of an integer
+// no decimals will be in the string
+
+// RETURNS
+// return number only
+
+// EXAMPLES
+// "1234" --> 1234
+// "605"  --> 605
+// "1405" --> 1405
+// "-7" --> -7
+
+// PSEUDOCODE
+// get string from input
+// use Number() method
+
+// SOLUTION
+const stringToNumber = str => Number(str)
+
+console.log(stringToNumber('1234'))
+
+// BEST SOLUTION
+// const stringToNumber = function(str){
+//     return parseInt(str);
+//   }
