ac67b633b784fb1bfc82f9eaa61763ead4e2fa64
patrickpiedad <patrickpiedad@gmail.com>
2024-07-12 14:38:04 +0200

    added codewars for 12JUL2024 8kyu JS

--- JS_codeWars_JUL24.js
+++ JS_codeWars_JUL24.js
@@ -502,11 +502,42 @@
 // use Number() method
 
 // SOLUTION
-const stringToNumber = str => Number(str)
+// const stringToNumber = str => Number(str)
 
-console.log(stringToNumber('1234'))
+// console.log(stringToNumber('1234'))
 
 // BEST SOLUTION
 // const stringToNumber = function(str){
 //     return parseInt(str);
 //   }
+
+///////////////////////////////codeWars 12JUL2024///////////////////////////////
+
+// TASK
+// In this kata you will create a function that takes in a list and returns a list with the reverse order.
+
+// Examples (Input -> Output)
+// * [1, 2, 3, 4]  -> [4, 3, 2, 1]
+// * [9, 2, 0, 7]  -> [7, 0, 2, 9]
+
+// PARAMETERS
+// taking in list as parameter
+// list will be an array of numbers only, no strings
+
+// RETURNS
+// return list in reverse order
+// allowed to mutate list or return new list
+
+// EXAMPLES
+// [1, 2, 3, 4]  -> [4, 3, 2, 1]
+// [9, 2, 0, 7]  -> [7, 0, 2, 9]
+
+// PSEUDOCODE
+// use method .reverse()
+
+// SOLUTION
+let reverseList = list => list.reverse()
+
+console.log(reverseList([1, 2, 3, 4]))
+
+// BEST SOLUTION
