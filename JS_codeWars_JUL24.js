1908ae8a14981b34356e2b99655720ec1159b7e6
patrickpiedad <patrickpiedad@gmail.com>
2024-07-13 11:04:48 +0200

    adding 13JUL2024 codewars, did not solve on own. need to study and put this into Anki

--- JS_codeWars_JUL24.js
+++ JS_codeWars_JUL24.js
@@ -536,8 +536,71 @@
 // use method .reverse()
 
 // SOLUTION
-let reverseList = list => list.reverse()
+// let reverseList = list => list.reverse()
 
-console.log(reverseList([1, 2, 3, 4]))
+// console.log(reverseList([1, 2, 3, 4]))
 
 // BEST SOLUTION
+
+///////////////////////////////codeWars 13JUL2024///////////////////////////////
+
+// TASK
+// Given an array of integers, find the one that appears an odd number of times.
+
+// There will always be only one integer that appears an odd number of times.
+
+// Examples
+// [7] should return 7, because it occurs 1 time (which is odd).
+// [0] should return 0, because it occurs 1 time (which is odd).
+// [1,1,2] should return 2, because it occurs 1 time (which is odd).
+// [0,1,0,1,0] should return 0, because it occurs 3 times (which is odd).
+// [1,2,2,3,3,3,4,3,3,3,2,2,1] should return 4, because it appears 1 time (which is odd).
+
+// PARAMETERS
+// array of integers will be passed into function
+// array will only be of integers, no strings
+// array will be a normal array, not an array of arrays
+// integers are defined as negative, positive, or zero
+// there will only be ONE integer that repeats an odd amount of times
+
+// RETURNS
+// return only the number that appears an odd amount of times
+// do not return an array, only the single number
+
+// EXAMPLES
+// [7] should return 7, because it occurs 1 time (which is odd).
+// [0] should return 0, because it occurs 1 time (which is odd).
+// [1,1,2] should return 2, because it occurs 1 time (which is odd).
+// [0,1,0,1,0] should return 0, because it occurs 3 times (which is odd).
+// [1,2,2,3,3,3,4,3,3,3,2,2,1] should return 4, because it appears 1 time (which is odd).
+
+// PSEUDOCODE
+// need to count the number of times an integer repeats in an array
+// then use that number with % 2 to determine whether it is odd, since there is only one integer that repeats an odd amount of times
+// use indexOf and lastIndexOf NOT being equal, which will get you values that repeat at some point, then use % to see if they are repeat an odd number of times
+
+// SOLUTION
+// function findOdd(A) {
+// 	for (let i = 0; i < A.length; i++) {
+// 		if (A.indexOf(A[i]) !== A.lastIndexOf(A)) {
+// 			A.map()
+// 		}
+// 	}
+// }
+
+let findOdd = A => A.find(thing => A.filter(elem => elem == thing).length % 2);
+
+console.log(findOdd([1, 2, 2, 3, 3, 3, 4, 3, 3, 3, 2, 2, 1]));
+
+// BEST SOLUTION
+// const findOdd = (xs) => xs.reduce((a, b) => a ^ b);
+
+// const findOdd: This line declares a constant variable findOdd and assigns it an arrow function. The function takes one parameter, xs, which represents the array of integers.
+
+// (a, b) => a ^ b: This part of the code is an arrow function used as a callback for the reduce method. The ^ operator in JavaScript performs a bitwise XOR operation. XOR (exclusive OR) returns 1 for bits that are different and 0 for bits that are the same. For example, 1 ^ 1 results in 0, and 1 ^ 0 results in 1.
+
+// xs.reduce((a, b) => a ^ b): Here, the reduce method is used on the input array xs. The reduce method takes a callback function that is applied to each element of the array. In this case, the callback function (a, b) => a ^ b calculates the XOR of the accumulated result a and the current element b. This operation is performed iteratively over the elements of the array until a single result is obtained.
+
+// The XOR operation has a special property: if you XOR a number with itself an odd number of times, the result will be the number itself. If you XOR a number with itself an even number of times, the result will be 0. Since all numbers in the array appear even times except for one, the XOR operation will eventually leave you with the number that appears an odd number of times.
+
+// In summary, this code calculates the XOR of all the elements in the array, effectively finding the number that appears an odd number of times, and returns that number as the result of the findOdd function.
