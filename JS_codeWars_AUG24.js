1eabb2702313f3080f4828fd41c7135ba38daa75
patrickpiedad <patrickpiedad@gmail.com>
2024-08-12 19:33:23 +0200

    added 12AUG2024 codewars 8kyu

--- JS_codeWars_AUG24.js
+++ JS_codeWars_AUG24.js
@@ -347,35 +347,74 @@
 // return all the saved portions
 
 // CODE
-function expandedForm(num) {
-	let arr = []
-	let numLength = num.toString().length
+// function expandedForm(num) {
+// 	let arr = []
+// 	let numLength = num.toString().length
 
-	for (let i = 0; i < numLength; i++) {
-		pushVal = num - (num % 10)
-		arr.push(pushVal)
-		// arr.push(num % 10)
-		num = num - (num - (num % 10))
-		console.log(num)
-	}
+// 	for (let i = 0; i < numLength; i++) {
+// 		pushVal = num - (num % 10)
+// 		arr.push(pushVal)
+// 		// arr.push(num % 10)
+// 		num = num - (num - (num % 10))
+// 		console.log(num)
+// 	}
 
-	return arr.join(' + ')
-}
+// 	return arr.join(' + ')
+// }
 
 // TEST
-console.log(expandedForm(12))
+// console.log(expandedForm(12))
 // console.log(expandedForm(42))
 // console.log(expandedForm(70304))
 
 // OPTIMIZE
 
 // BEST SOLUTION
-const expandedForm = n =>
-	n
-		.toString()
-		.split('')
-		.reverse()
-		.map((a, i) => a * Math.pow(10, i))
-		.filter(a => a > 0)
-		.reverse()
-		.join(' + ')
+// const expandedForm = n =>
+// 	n
+// 		.toString()
+// 		.split('')
+// 		.reverse()
+// 		.map((a, i) => a * Math.pow(10, i))
+// 		.filter(a => a > 0)
+// 		.reverse()
+// 		.join(' + ')
+
+///////////////////////////////codeWars 12AUG2024///////////////////////////////
+
+// TASK
+// Your task is to find the first element of an array that is not consecutive.
+
+// By not consecutive we mean not exactly 1 larger than the previous element of the array.
+
+// E.g. If we have an array [1,2,3,4,6,7,8] then 1 then 2 then 3 then 4 are all consecutive but 6 is not, so that's the first non-consecutive number.
+
+// If the whole array is consecutive then return null.
+
+// The array will always have at least 2 elements1 and all elements will be numbers. The numbers will also all be unique and in ascending order. The numbers could be positive or negative and the first non-consecutive could be either too!
+
+// 1 Can you write a solution that will return null for both [] and [ x ] though? (This is an empty array and one with a single number and is not tested for, but you can write your own example test. )
+
+// REPEAT
+
+// EXAMPLES
+
+// APPROACH
+
+// CODE
+function firstNonConsecutive(arr) {
+	for (let i = 0; i < arr.length - 1; i++) {
+		if (arr[i] !== arr[i + 1] - 1) {
+			return arr[i + 1]
+		}
+	}
+	return null
+}
+
+// TEST
+console.log(firstNonConsecutive([1, 2, 3, 4, 6, 7, 8]))
+console.log(firstNonConsecutive([1, 2, 3, 4, 5, 6, 7, 8]))
+
+// OPTIMIZE
+
+// BEST SOLUTION
