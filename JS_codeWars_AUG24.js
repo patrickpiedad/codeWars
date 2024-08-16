979df48a2f1e9684e3c67f21bc21aece89a87063
patrickpiedad <patrickpiedad@gmail.com>
2024-08-16 11:06:46 +0200

    adding 16AUG2024 codewars, 8kyu

--- JS_codeWars_AUG24.js
+++ JS_codeWars_AUG24.js
@@ -444,18 +444,48 @@
 // of its absolute value decimal places.
 
 // CODE
-function sumDigits(number) {
-	if (number === 0) {
+// function sumDigits(number) {
+// 	if (number === 0) {
+// 		return 0
+// 	}
+// 	let numberArr = Math.abs(number).toString().split('')
+// 	return numberArr.reduce((acc, currVal) => Number(acc) + Number(currVal))
+// }
+
+// TEST
+// console.log(sumDigits(10))
+// console.log(sumDigits(99))
+// console.log(sumDigits(-32))
+
+// OPTIMIZE
+
+// BEST SOLUTION
+
+///////////////////////////////codeWars 16AUG2024///////////////////////////////
+
+// TASK
+// Create a combat function that takes the player's current health and the amount of damage recieved, and returns the player's new health. Health can't be less than 0.
+
+// REPEAT
+// I am getting a player's current health and the amount of damage received, then returning the new player's health.
+
+// EXAMPLES
+// health 100, damage 5, return 95
+// health 20, damage 30, return 0 (edge case)
+
+// APPROACH
+// subtraction with conditional for edge case
+
+// CODE
+function combat(health, damage) {
+	if (health >= damage) {
+		return health - damage
+	} else if (damage > health) {
 		return 0
 	}
-	let numberArr = Math.abs(number).toString().split('')
-	return numberArr.reduce((acc, currVal) => Number(acc) + Number(currVal))
 }
 
 // TEST
-console.log(sumDigits(10))
-console.log(sumDigits(99))
-console.log(sumDigits(-32))
 
 // OPTIMIZE
 
